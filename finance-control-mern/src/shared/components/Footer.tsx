cf146114b4b59becc0c3af0f0b44f34d5f541c23
diogogaio <diogogaio@gmail.com>
2024-10-22 15:52:28 -0300

    New version deployed

--- finance-control-mern/src/shared/components/Footer.tsx
+++ finance-control-mern/src/shared/components/Footer.tsx
@@ -31,7 +31,7 @@ export const Footer = () => {
       {showDevName && (
         <Tooltip
           sx={{ fontSize: "3px" }}
-          title="20241007.1407"
+          title="20241022.1550"
           placement="top-end"
         >
           <Typography variant="caption" sx={{ fontStyle: "italic" }}>
